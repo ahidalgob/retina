3952bcbdf6dd23c3b68e09bdaf9634bca0318433
ahidalgob <ahidalgo94@gmail.com>
2017-03-23 04:28:03 -0400

    Conazos con graphics

--- graphics/intento.hs
+++ graphics/intento.hs
@@ -3,13 +3,15 @@ import Graphics.UI.GLUT
 
 import Graphics.UI.GLUT.Window
 
-
 import Data.Word
 import Foreign
 import Graphics.Rendering.OpenGL
 
+import Graphics.Rendering.OpenGL.GL.PixelRectangles.PixelStorage
 
 import Data.Bits
+
+
 
 readPixelArray :: Int -> Int -> Int -> Int -> IO [Word32]
 readPixelArray x y w h = do
@@ -20,7 +22,7 @@ readPixelArray x y w h = do
         -- fromIntegral is needed because Position and Size store GLints not Ints
         let position = Position (fromIntegral x) (fromIntegral y)
         let size = Size (fromIntegral w) (fromIntegral h)
-        readPixels position size $ PixelData RGBA UnsignedInt ptr
+        readPixels position size $ PixelData RGBA UnsignedByte ptr
         peekArray arraySize ptr
 
 
@@ -40,13 +42,26 @@ pixelFromWord32 colour = Pixel (extract 3) (extract 2) (extract 1) (extract 0)
 
 
 
+myinit :: IO ()
+myinit = do
+    drawBuffer $= FrontAndBackBuffers
+    readBuffer $= FrontAndBackBuffers
+    -- glClearColor(0.0, 0.0, 0.0, 0.0);
+    rowAlignment Unpack $= 1
+    viewport $= (Position 0 0, Size 100 100)
+    matrixMode $= Projection
+    loadIdentity
+    matrixMode $= Modelview 4
+
 main :: IO ()
 main = do
   -- Initialize OpenGL via GLUT
   (progname, _) <- getArgsAndInitialize
   initialDisplayMode $= [DoubleBuffered,RGBAMode]
-  initialWindowSize $= Size 500 500
+  initialWindowSize $= Size 100 100
+  initialWindowPosition $= Position 700 100
 
+  myinit
 
   -- Create the output window
   createWindow progname
@@ -56,26 +71,28 @@ main = do
   -- Let GLUT handle the window events, calling the displayCallback as fast as it can
   mainLoop
 
+
+
 circle (x, y) radius divs = map toPoint angles where 
     arc       = (pi/2) / fromIntegral divs
     toPoint a = (x + cos a * radius, y + sin a * radius)
     angles    = map ((*arc) . fromIntegral) [0..divs]
 
+
 display :: IO ()
 display = do
   -- Clear the screen with the default clear color (black)
   clear [ ColorBuffer ]
+  loadIdentity
+  let points = circle (0,0) (1) 5 :: [(GLfloat, GLfloat)]
 
-  let points = circle (0,0) (1) 500 :: [(GLfloat, GLfloat)]
-
-  renderPrimitive LineStrip $ do
-   mapM_ (\(x, y) -> vertex $ Vertex2 x y) points
+  renderPrimitive Polygon $ do
+    mapM_ (\(x, y) -> vertex $ Vertex2 x y) points
 
 
   swapBuffers
-  a <- readPixels' (0) (0) 5 5
+  a <- readPixels' (0) (0) 100 100
   mapM_ print a
-  
 
   -- Send all of the drawing commands to the OpenGL server
   --flush
